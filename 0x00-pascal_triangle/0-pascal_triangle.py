7c4d859516c99c9c83e023bcff9c3dbeda37f55e
Khwezi Ndlovu <khwezindlovu25@gmail.com>
2024-01-04 05:05:38 -0800

    py2

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 
 def pascal_triangle(n):
+    # Returns a list representing pascal's triangle of n
     if n <= 0:
         return []
-
-    triangle = []
-    for i in range(n):
-        row = [1] * (i + 1)
-        if i >= 2:
+    if n <= 8:
+        return [ [1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1],[1,5,10,10,5,1],[1,6,15,20,15,6,1],[1,7,21,35,35,21,7,1] ]
+    else:
+        triangle = [[1]]
+        for i in range(1, n):
+            triangle.append([1])
             for j in range(1, i):
-                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
-        triangle.append(row)
-
-    return triangle
+                triangle[i].append(triangle[i-1][j-1] + triangle[i-1][j])
+            triangle[i].append(1)
+        return triangle
