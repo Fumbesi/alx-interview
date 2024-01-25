646de1a65281c23c402042ffdb56e0b0937c2215
Khwezi Ndlovu <khwezindlovu25@gmail.com>
2024-01-25 11:17:33 -0800

    0

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,41 +1,45 @@
 #!/usr/bin/python3
-import sys
+"""This script reads lines from stdin in this format
+<IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>
+and after every 10 lines or keyboard interruption
+it prints File size: <total size>
+<status code>: <number> for every status code"""
 
-def print_statistics(total_size, status_code_counts):
-    print("File size: {}".format(total_size))
-    for code in sorted(status_code_counts):
-        print("{}: {}".format(code, status_code_counts[code]))
+from sys import stdin
+import re
 
-def parse_line(line):
-    try:
-        parts = line.split()
-        ip_address = parts[0]
-        status_code = int(parts[-2])
-        file_size = int(parts[-1])
-        return ip_address, status_code, file_size
-    except (ValueError, IndexError):
-        return None, None, None
 
-def main():
-    lines_processed = 0
+def valid_format(line):
+    """checks if the line have a valid format"""
+    pattern = r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - '
+    r'\[([^\]]+)\] "GET \/projects\/260 HTTP\/1\.1" (\d{3}) (\d+)$'
+    match = re.match(pattern, line)
+    if match:
+        return True
+    return False
+
+
+try:
+    my_dict = {}
     total_size = 0
-    status_code_counts = {}
-
-    try:
-        for line in sys.stdin:
-            ip_address, status_code, file_size = parse_line(line)
-            if ip_address is not None:
-                lines_processed += 1
-                total_size += file_size
-                status_code_counts[status_code] = status_code_counts.get(status_code, 0) + 1
-
-            if lines_processed % 10 == 0:
-                print_statistics(total_size, status_code_counts)
-
-    except KeyboardInterrupt:
-        # Handle keyboard interruption
-        print_statistics(total_size, status_code_counts)
-        sys.exit(0)
-
-if __name__ == "__main__":
-    main()
+    for i, line in enumerate(stdin, start=1):
+        line = line.strip()
+        if not valid_format(line):
+            continue
+        parts = line.split(" ")
+        total_size += int(parts[-1])
+        if parts[-2] not in my_dict:
+            my_dict[parts[-2]] = 1
+        else:
+            my_dict[parts[-2]] += 1
+        my_dict = dict(sorted(my_dict.items()))
+        if i % 10 == 0:
+            print("File size: {}".format(total_size))
+            for key, val in my_dict.items():
+                print("{}: {}".format(key, val))
+except Exception as err:
+    pass
+finally:
+    print("File size: {}".format(total_size))
+    for key, val in my_dict.items():
+        print("{}: {}".format(key, val))
