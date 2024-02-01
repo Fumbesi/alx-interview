7fb515f345818cdd45c3e8a304ccd9a253b4eab7
Fumbesi <124091057+Fumbesi@users.noreply.github.com>
2024-02-01 19:15:46 +0200

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,27 +1,52 @@
 #!/usr/bin/python3
+'''LockBoxes Challenge'''
+from sys import setrecursionlimit
+
 
 def canUnlockAll(boxes):
-    if not boxes or not isinstance(boxes, list):
+    '''determines if all the boxes can be opened or not
+    Returns:
+        True: all boxes can be opened
+        False: not all boxes can be opened
+    '''
+    initial_index = 0
+    keys = {0}
+    checkboxes(boxes, initial_index, keys)
+    zero_in = False
+    for i in boxes:
+        if 0 in i:
+            zero_in = True
+
+    if len(keys) > len(boxes) and zero_in:
         return False
 
-    n = len(boxes)
-    if n == 0:
-        return False
+    i = 0
+    while i < len(keys):
+        if i <= len(keys) - 2 and list(keys)[i + 1] != list(keys)[i] + 1:
+            return False
+        i += 1
+    return True
 
-    keys = set([0])  # Start with keys to the first box
 
-    for key in keys:
-        box = boxes[key]
-        keys.update(box)
+def checkboxes(boxes, pos, keys):
+    '''
+    The function "checkboxes" recursively checks if all checkboxes
+    can be selected based on the given positions and keys.
+    '''
+    setrecursionlimit(10000)
+    if len(keys) == len(boxes):
+        return True
 
-    return len(keys) == n
+    # if calls >= len(boxes):
+    #     return keys
 
-# Example usage
-boxes1 = [[1], [2], [3], [4], []]
-print(canUnlockAll(boxes1))  # True
+    if pos >= len(boxes):
+        return
 
-boxes2 = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
-print(canUnlockAll(boxes2))  # True
-
-boxes3 = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-print(canUnlockAll(boxes3))  # False
+    for i in boxes[pos]:
+        if i in keys:
+            continue
+        keys.add(i)
+        # print(f"pos = {pos}, i = {i}, keys = {keys}")
+        checkboxes(boxes, i, keys)
+    return keys
